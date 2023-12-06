c8f8eeff357afcce2c9dbcec2cd23b1d2640208e
squidmin <morse.james.r@gmail.com>
2023-12-05 22:15:56 -0600

    Refactor main function to maintain list of subcommand functions

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 	"os"
 )
 
+// Define a type for a function that returns a *cobra.Command
+type commandFunc func() *cobra.Command
+
 func main() {
 	rootCmd := newRootCmd()
-	rootCmd.AddCommand(newHelloCmd())
-	rootCmd.AddCommand(newGoodbyeCmd())
+
+	// List of functions that return subcommands
+	commands := []commandFunc{
+		newHelloCmd,
+		newGoodbyeCmd,
+	}
+
+	// Iterate over the slice and add each command to the root command
+	for _, cmdFunc := range commands {
+		rootCmd.AddCommand(cmdFunc())
+	}
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
